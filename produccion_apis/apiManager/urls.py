570426d1b305868b0fe83be93759bffbf1dc5dc2
EmilianoRios <emiliano_luisrios@hotmail.com>
2022-05-02 20:30:32 -0300

    PrimerCommit-Test

---
+++ produccion_apis/apiManager/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from apiManager import views
+
+urlpatterns = [
+    path('',views.home,name='HomeFreeToGame'),
+    path('games/<str:plataform>/',views.gamesList,name='Games'),
+    path('games/<str:plataform>/<str:genre>/',views.gamesList,name='GamesG'),
+    path('game/<int:game>/',views.game,name='Game'),
+    path('myGames/',views.myGames,name='MyGames'),
+    path('search/',views.searchGames,name='SearchGames'),
+]
